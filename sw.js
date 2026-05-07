fb9aedb119e646a210dbaed707ced8aa74870bb9
beastwjf <183525387@qq.com>
2026-05-07 13:08:43 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'longmen-invest-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/data.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(
+        names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))
+      )
+    )
+  );
+});
